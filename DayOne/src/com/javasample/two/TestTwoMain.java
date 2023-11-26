e0ab1197daf3fff9b99baddfe5c565aa99335703
Anand <anand@metalnetworks.com>
2023-11-26 19:54:00 +0530

    Example for For Loop

---
+++ DayOne/src/com/javasample/two/TestTwoMain.java
@@ -0,0 +1,12 @@
+package com.javasample.two;
+
+public class TestTwoMain {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=10;i++) {
+			
+			System.out.println("Count  i="+i);
+		}
+	}
+
+}
